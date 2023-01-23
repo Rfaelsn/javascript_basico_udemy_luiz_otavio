e97afc9778193bcf709639034fd1cc83a584ed59
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-01-23 19:30:35 -0300

    exercício Switch case

---
+++ codigo_aulas/aula_29_switch_case/FuncaoDiaSemana.js
@@ -0,0 +1,43 @@
+function definirDiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';    
+            return diaSemanaTexto;
+    
+        case 1:
+            diaSemanaTexto = 'Segunda';    
+            return diaSemanaTexto;
+    
+        case 2:
+            diaSemanaTexto = 'Terça';    
+            return diaSemanaTexto;
+    
+        case 3:
+            diaSemanaTexto = 'Quarta';    
+            return diaSemanaTexto;
+    
+        case 4:
+            diaSemanaTexto = 'Quinta';    
+            return diaSemanaTexto;
+    
+        case 5:
+            diaSemanaTexto = 'Sexta';    
+            return diaSemanaTexto;
+    
+        case 6:
+            diaSemanaTexto = 'Sábado';    
+            return diaSemanaTexto;
+    
+        default:
+            diaSemanaTexto = 'dia Inválido';
+            return diaSemanaTexto;
+    }
+}
+
+const data = new Date('2001-04-18 18:00:00');
+const diaSemana = data.getDay();
+const diaSemanaTexto = definirDiaSemanaTexto(diaSemana);
+
+console.log(diaSemanaTexto);
