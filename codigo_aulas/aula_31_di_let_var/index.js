75777a8f62df75aeb67fd9360984644cf868f8e1
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-01-26 19:35:06 -0300

    explorando as diferenças de let e var

---
+++ codigo_aulas/aula_31_di_let_var/index.js
@@ -0,0 +1,33 @@
+const verdadeira = true;
+
+/* let nome = 'Rafael';
+var nome2 = 'Rafael';
+
+if (verdadeira) {
+    let nome = 'Nascimento';
+    console.log(nome,nome2);
+
+    if (verdadeira) {
+        var nome2 = 'outra coisa';
+        let nome = 'Nascimento';
+        console.log(nome,nome2);
+    }
+} 
+
+console.log(nome,nome2);
+
+const nomeNaFuncao = 'Rafael função';
+function falaOi(){
+    console.log(nomeNaFuncao);
+    var nomeDaFuncao = 'não pode ser acessada';
+}
+
+console.log(nomeDaFuncao); */
+
+// não sofremb hosting
+const func = function(){
+
+}
+//ex hosting
+console.log(nome);
+let nome = 'nome';  
