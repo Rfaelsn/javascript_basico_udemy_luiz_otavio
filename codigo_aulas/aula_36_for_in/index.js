d29e08a08f084a37c92f4d3088bdc7f9b00e882d
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-01-29 18:59:15 -0300

    testando estrutura for in

---
+++ codigo_aulas/aula_36_for_in/index.js
@@ -0,0 +1,17 @@
+//for in com Objeto
+const pessoa = {
+    nome:'Rafael',
+    idade: 21
+};
+
+for (const key in pessoa) {
+    console.log(key, pessoa[key]);
+}
+
+
+//for in com array:
+// const frutas = ['Banana', 'Laranja', 'Melância'];
+
+// for (const index in frutas) {
+//     console.log(frutas[index]);
+// }
