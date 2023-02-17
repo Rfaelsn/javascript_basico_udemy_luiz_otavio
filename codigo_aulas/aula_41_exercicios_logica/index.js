313532f873b0425ac925c5b584e8e51e040f8d79
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-02-17 00:34:23 -0300

    exercício retorna maior numero

---
+++ codigo_aulas/aula_41_exercicios_logica/index.js
@@ -0,0 +1,17 @@
+// function retornaMaiorNumero(num1, num2){
+//     const maior = num1 > num2 ? num1 : num2;
+//     return maior;
+// }
+
+// const maiorNumero = retornaMaiorNumero(5, 2);
+// console.log(maiorNumero);
+
+//com arrow Function:
+// const maior = (num1, num2) =>{
+//     return num1 > num2 ? num1 : num2;
+// }
+
+//arrow Function simplificado
+const maiorNumero = (num1, num2) => num1 > num2 ? num1 : num2;
+
+console.log(maiorNumero(1,5));
