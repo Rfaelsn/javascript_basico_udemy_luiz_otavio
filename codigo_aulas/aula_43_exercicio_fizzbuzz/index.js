ae4cd25aefafc429963c06aa8a2a5b6a72a48a61
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-02-17 01:28:27 -0300

    exercício fizzbuzz

---
+++ codigo_aulas/aula_43_exercicio_fizzbuzz/index.js
@@ -0,0 +1,23 @@
+const resultFizzBuzz = (numero) =>{
+    if (typeof numero !== 'number'){
+        return numero;
+    }
+
+    if(numero%3 === 0 && numero%5 === 0){
+        return 'fizzBuzz';
+    }
+    if(numero%3 === 0){
+        return 'Fizz';
+    }
+    
+    if(numero%5 === 0){
+        return 'Buzz';
+    }
+
+    return numero;
+    
+}
+
+for(let i=0; i<100; i++){
+    console.log(resultFizzBuzz(i));
+}
