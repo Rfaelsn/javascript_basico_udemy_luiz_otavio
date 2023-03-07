33f649c27eca6fe10497c01fe1557d68d0fea9d5
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-03-06 23:00:36 -0300

    factory functions final commit

--- codigo_aulas/aula_56_factory_functions/index.js
+++ codigo_aulas/aula_56_factory_functions/index.js
@@ -5,6 +5,18 @@ function criaPessoa(nome, sobrenome, altura, peso){
         sobrenome,
         altura,
         peso,
+
+        get nomeCompleto(){
+            return `${this.nome} ${this.sobrenome}`
+        },
+
+        set nomeCompleto(valor){
+            valor = valor.split(' ');
+            this.nome = valor.shift();//retira do começo do array e retorna o valor
+
+            // insere os valores do array em apenas um valor string separado pelo argumento passado
+            this.sobrenome = valor.join(' ');
+        },
         
         fala(assunto){
             //return `${this.nome} está conservanso sobre ${assunto}` // mesmo efeito porém menos performance
@@ -23,4 +35,7 @@ const p1 = criaPessoa('João', 'Silva', 1.75, 80);
 const p2 = criaPessoa('Maria', 'Silva', 1.80, 60);
 
 console.log(p1.fala("computação"));
+//usando set e get, as funções se comportam como atributos
+p1.nomeCompleto = 'Rafael Silva do Nascimento';
+console.log(p1.nomeCompleto);
 console.log(`o imc de ${p2.nome} é ${p2.imc}`);
