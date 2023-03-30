1f2f8a055a63d42195b7cd1dd322acb7bac24993
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-03-30 00:47:02 -0300

    utilizando filter+map+reduce em um unico array

---
+++ codigo_aulas/aula_68_filter_map_reduce/index.js
@@ -0,0 +1,11 @@
+//retorne a soma do dobro de todos os pares.
+const numeros = [5, 50 , 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+const somaDobroPares = numeros
+    .filter(valor => valor%2 === 0)
+    .map(valor => valor*2)
+    .reduce((acc, valor) => acc + valor)
+
+console.log(somaDobroPares);
+
+
